7f11db2edfd4246acf1166afa016fbd6480399e8
Larien27 <lucie.beke@gmail.com>
2024-06-08 20:43:23 +0200

    Language text value now automatically assigned and minor CSS change in About page

--- portfolio/src/components/pieces/LanguageBox.js
+++ portfolio/src/components/pieces/LanguageBox.js
@@ -1,36 +1,45 @@
 import './LanguageBox.css';
 
-const getLevelValue = (level) => {
+const getLevelInfo = (level) => {
     let levelValue;
+    let levelText;
 
     switch (level) {
         case 'A1':
             levelValue = 1;
+            levelText = 'Beginner';
             break;
         case 'A2':
             levelValue = 2;
+            levelText = 'Elementary';
             break;
         case 'B1':
             levelValue = 3;
+            levelText = 'Intermediate';
             break;
         case 'B2':
             levelValue = 4;
+            levelText = 'Upper-Intermediate';
             break;
         case 'C1':
             levelValue = 5;
+            levelText = 'Advanced';
             break;
         case 'C2':
             levelValue = 6;
+            levelText = 'Proficient';
             break;
         default:
             levelValue = 0;
+            levelText = 'Unknown';
     }
 
-    return levelValue;
+    return [levelValue, levelText];
 }
 
-const LanguageBox = ({ language, flagFileName, flagAltText, level, levelText }) => {
-    const levelValue = getLevelValue(level);
+const LanguageBox = ({ language, flagFileName, flagAltText, level }) => {
+    const levelValue = getLevelInfo(level)[0];
+    const levelText = getLevelInfo(level)[1];
     
     return (
         <li className="language-box">
