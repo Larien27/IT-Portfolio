72e8d1f2ed5ab65de733db37759b096cf6e0d28c
Larien27 <lucie.beke@gmail.com>
2024-06-19 21:56:36 +0200

    Added style changes to the main page

--- portfolio/src/pages/Main/MainPage.js
+++ portfolio/src/pages/Main/MainPage.js
@@ -3,14 +3,16 @@ import './MainPage.css';
 const MainPage = () => {
     return (
         <div id="main" className="section">
-            <h1>Lucie Bekeová</h1>
-            <span id="subtitle">Junior web developer</span>
+            <div className="title">
+                <h1>Lucie Bekeová</h1>
+                <span id="subtitle">Junior web developer</span>
+            </div>
             <div id="text-photo-section">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec malesuada ante. Donec vehicula ultricies diam, tempor mattis justo pharetra vitae. Mauris varius est at mi venenatis consectetur. Nunc sit amet ante lacinia, finibus nulla imperdiet, sollicitudin enim. Vivamus hendrerit arcu et ipsum laoreet, ac pretium quam gravida. In scelerisque sollicitudin mauris nec ornare. Nunc aliquet sapien ut leo lacinia, in auctor neque facilisis. Sed id venenatis metus, vel blandit nisi. Aliquam pulvinar faucibus sodales.</p>
                 <img src="https://placehold.co/400x300" alt="placeholder img" />
             </div>
             <div id="link-buttons">
-                <a href="/about" className="link-button">Something about me</a>
+                <a href="/about" className="link-button">More about me</a>
                 <a href="/projects" className="link-button">Something about my projects</a>
                 <a href="/contact" className="link-button">How to contact me</a>
             </div>
