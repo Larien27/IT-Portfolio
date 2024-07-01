e1f4c911459ad909786dd97a0d3a52556ec1ec9d
Larien27 <lucie.beke@gmail.com>
2024-07-01 17:50:03 +0200

    Project image is visible only if there is any

--- portfolio/src/pages/Projects/Projects.js
+++ portfolio/src/pages/Projects/Projects.js
@@ -44,7 +44,9 @@ const Projects = () => {
                             <h2>{projectDetail.projectName} ({projectDetail.year})</h2>
                             <p>{projectDetail.shortDescription}</p>
                             <p>Used technologies: {projectDetail.technologies}</p>
-                            <img src={`${process.env.PUBLIC_URL}/images/projects/${projectDetail.imageFileName}`} alt={`${projectDetail.projectName} overview`} />
+                            {projectDetail.imageFileName && (
+                                <img src={`${process.env.PUBLIC_URL}/images/projects/${projectDetail.imageFileName}`} alt={`${projectDetail.projectName} overview`} />
+                            )}
                             <p className="long-description-text">{projectDetail.longDescription}</p>
                         </div>
                     ) : "Choose a project to see details"}
