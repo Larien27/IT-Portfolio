1a8aaf7337be95eaac7478a81b35d2ed8d392ced
Larien27 <lucie.beke@gmail.com>
2024-07-13 22:08:47 +0200

    Mobile navbar

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -6,9 +6,12 @@ const Navbar = () => {
         <nav id="navbar">
             <span><Link to="/">Lucie Bekeová</Link></span>
             <ul>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li className="desktop"><Link to="/about">About</Link></li>
+                <li className="mobile"><Link to="/about"><img src={`${process.env.PUBLIC_URL}/images/about.png`} className="icon" alt="About icon" /></Link></li>
+                <li className="desktop"><Link to="/projects">Projects</Link></li>
+                <li className="mobile"><Link to="/projects"><img src={`${process.env.PUBLIC_URL}/images/projects.png`} className="icon" alt="Projects icon" /></Link></li>
+                <li className="desktop"><Link to="/contact">Contact</Link></li>
+                <li className="mobile"><Link to="/contact"><img src={`${process.env.PUBLIC_URL}/images/contact.png`} className="icon" alt="Contact icon" /></Link></li>
             </ul>
         </nav>
     );
